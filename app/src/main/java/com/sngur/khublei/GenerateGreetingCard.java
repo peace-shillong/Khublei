cd1f96cb240d80228064f19dad7c6b48a3bde514
peace-shillong <bwidu.peace@gmail.com>
2020-08-26 12:22:40 +0530

    ANDROIDX

--- app/src/main/java/com/sngur/khublei/GenerateGreetingCard.java
+++ app/src/main/java/com/sngur/khublei/GenerateGreetingCard.java
@@ -1,17 +1,14 @@
 package com.sngur.khublei;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
+
+import androidx.core.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
